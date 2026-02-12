cc77f82dbc5dffbc562bfceb91369cdeaa3b3f93
artemws <59208085+artemws@users.noreply.github.com>
2026-02-12 21:29:54 +0200

    Add MikroTik 7 CHR installation script
    This script installs MikroTik 7 CHR and sets up the environment.

---
+++ mik_chr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+echo
+echo "=== Artemws ==="
+echo "=== https://github.com/artemws ==="
+echo "=== MikroTik 7 CHR & Container Installer ==="
+echo
+sleep 3
+wget https://github.com/artemws/Install-MikroTik-CHR-on-VPS/raw/refs/heads/main/chr_7_16_x.7z -O chr_7_16_x.7z  && \
+apt update && \
+apt install p7zip-full && \
+7za x chr_7_16_x.7z && \
+STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
+echo STORAGE is $STORAGE && \
+ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
+echo ETH is $ETH && \
+ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
+echo ADDRESS is $ADDRESS && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo GATEWAY is $GATEWAY && \
+sleep 5 && \
+dd if=chr_7_16_x.img of=/dev/$STORAGE bs=4M oflag=sync && \
+echo "Ok, reboot" && \
+echo 1 > /proc/sys/kernel/sysrq && \
+echo b > /proc/sysrq-trigger && \
